a7e2cfe7aa14719f36c32545a25f15b7f426aa7e
Thoriq Firdaus <thoriqoe@gmail.com>
2018-07-29 06:22:16 +0700

    Add PHPCS into pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,5 +7,6 @@ source "$(dirname "$0")/shared.sh"
 # Check PHP files staged in the
 PHP_STAGED_FILES=$(diff_files --ext=".php")
 if [[ ! -z $PHP_STAGED_FILES ]]; then
+	test_phpcs
 	test_phpunit
 fi
