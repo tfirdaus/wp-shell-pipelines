7e51e8cff4e5e2390ffd840dd027730d49e267e8
Thoriq Firdaus <thoriqoe@gmail.com>
2019-07-01 13:06:04 +0700

    Add phpcbf

--- phpcs.sh
+++ phpcs.sh
@@ -3,7 +3,7 @@
 run_phpcs() {
 
 	if [[ $(vendor/bin/phpcs --version) ]]; then
-		vendor/bin/phpcs --extensions=php .
+		./vendor/bin/phpcs --extensions=php .
   	elif [[ $(phpcs --version) ]]; then
 		phpcs --extensions=php .
 	elif [[ $(composer --version) ]]; then
@@ -13,6 +13,29 @@ run_phpcs() {
 
 		composer global require "squizlabs/php_codesniffer=*"
 		"$(composer global config home)"/vendor/bin/phpcs --version
+		"$(composer global config home)"/vendor/bin/phpcs --extensions=php .
   fi
 }
-run_phpcs
+
+run_phpcbf() {
+
+	if [[ $(vendor/bin/phpcbf --version) ]]; then
+		./vendor/bin/phpcbf --extensions=php .
+  	elif [[ $(phpcbf --version) ]]; then
+		phpcbf --extensions=php .
+	elif [[ $(composer --version) ]]; then
+
+		echo -e "\\nℹ️ PHPCBF could not be found locally or globally, but Composer is available."
+		echo "🔄 Installing PHPCBF through Composer..."
+
+		composer global require "squizlabs/php_codesniffer=*"
+		"$(composer global config home)"/vendor/bin/phpcbf --version
+		"$(composer global config home)"/vendor/bin/phpcbf --extensions=php .
+  fi
+}
+
+if [[ $1 == '--fix' ]]; then
+	run_phpcbf
+else
+	run_phpcs
+fi
